b5582d6c73fd3afeb97db8547ab5c65fff01b4a8
Theoriginal007 <lauragachanja007@gmail.com>
2024-09-30 21:49:25 +0300

    Changes

---
+++ src/utils/helper.py
@@ -0,0 +1,11 @@
+def greet_user(username):
+    """Greets the user with their username."""
+    return f"Hello, {username}! Welcome to the application."
+
+def calculate_square(number):
+    """Returns the square of a given number."""
+    return number ** 2
+
+def is_even(number):
+    """Checks if a number is even."""
+    return number % 2 == 0

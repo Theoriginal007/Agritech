cd9d2f1e247ab9367676cf9894045726cb70540d
Theoriginal007 <lauragachanja007@gmail.com>
2024-09-30 21:50:18 +0300

    Changes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from module1 import add, subtract
 from module2 import multiply, divide
+from utils.helper import greet_user, calculate_square, is_even
 
 def main():
     a, b = 10, 5
+    username = "User"
 
+    print(greet_user(username))
+    print(f"Square of {a}: {calculate_square(a)}")
+    print(f"{a} is even: {is_even(a)}")
+    
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
     print(f"Multiplication of {a} and {b}: {multiply(a, b)}")
